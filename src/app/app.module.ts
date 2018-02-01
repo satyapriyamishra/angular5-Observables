1d2208660d56db0c2289e69a3ac09783de6ffc1a
Satyapriya <satyapriyamishra111.com>
2018-02-01 22:45:01 +0530

    creating two components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
+import { HttpModule} from "@angular/http";
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
